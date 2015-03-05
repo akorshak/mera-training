225c9ba22e96b660b55c253b0d27860fffe53dfe
unknown <atumarin@n103538.merann.ru>
2015-03-05 13:57:14 +0300

    Drawing app was added

---
+++ tumarinson/DragAndDraw/src/com/example/draganddraw/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.draganddraw;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+
+	
+}
